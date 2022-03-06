986debaaf9b0ae116e87c46cd593f500eb9df96e
usersaras <saras.sthapitshrestha@gmail.com>
2022-03-06 22:18:14 +0545

    added two sections

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        margin: 900,
+        items: 1,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        loop: true
+        // reponsive:{
+        //     0 : {
+        //         items: 1
+        //     },
+        //     600 : {
+        //         items: 1
+        //     }
+        // }
+    });
+  });
